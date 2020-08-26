df47f6b76941575706496ef90afece0cfa902765
Daria D <darja.dolbe@gmail.com>
2020-08-26 21:50:54 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace Knight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Knight = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Knight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Knight
+            // 
+            this.Knight.Location = new System.Drawing.Point(320, 102);
+            this.Knight.Name = "Knight";
+            this.Knight.Size = new System.Drawing.Size(150, 250);
+            this.Knight.TabIndex = 0;
+            this.Knight.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Knight);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Knight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Knight;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Knight
+{
+    public partial class Form1 : Form
+    {
+
+        int frameCounter = 1;
+        string direction = "right";
+        int knightStep = 7;
+        int horizontalVelocity = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeKnight();
+        }
+
+        private void InitializeKnight()
+        {
+            Knight.BackColor = Color.Transparent;
+            timer1.Interval = 200;
+            timer1.Start();
+            this.KeyDown += Form1_KeyDown;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string imageName = "knight_" + direction + "_" + frameCounter.ToString();
+            Knight.Image = (Image)Properties.Resources.ResourceManager.GetObject(imageName);
+            Knight.SizeMode = PictureBoxSizeMode.StretchImage;
+            frameCounter++;
+            if (frameCounter > 2)
+            {
+                frameCounter = 1;
+            }
+            Knight.Left += horizontalVelocity;
+        }
+
+        private void Form1_KeyDown (object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right:
+                    direction = "right";
+                    horizontalVelocity = knightStep;
+                    break;
+               
+                case Keys.Left:
+                    direction = "left";
+                    horizontalVelocity = -knightStep;
+                    break;
+                
+            }
+        }
+    }
+
+}
